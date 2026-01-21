2bed78f22d43586c4d4b6440972b9db97d78451e
jaehakl <jaehak@qutat.net>
2026-01-21 17:26:35 +0900

    torch

--- apps/imagene/api/run.bat
+++ apps/imagene/api/run.bat
@@ -1,6 +1,8 @@
 cd ./app
 poetry run uvicorn main:app --reload --host 0.0.0.0
-
+rem cd ./app
+rem call ./_creator/.venv/Scripts/activate.bat
+rem uvicorn _creator_main:app --reload --host 0.0.0.0
 
 
 
